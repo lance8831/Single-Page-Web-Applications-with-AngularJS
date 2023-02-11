606c65fd1c7ef0b4dabfec9408ce6c8136b0ac67
lance8831 <leezhenhao88@gmail.com>
2023-02-11 20:49:50 +0900

    ongoing

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -2,15 +2,26 @@
 'use strict';
     
 angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+ .controller('LunchCheckController', LunchCheckController);
 
 LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope) {
+function LunchCheckController ($scope){ 
+    $scope.foodList = "";
+    $scope.numOfFood = 0;
+    
+    $scope.displayNumOfFood = function () {
+        var totalFood = foodCount($scope.foodList);
+        $scope.numOfFood = totalFood;     
+    };
+    
+    function foodCount (str){
+        var totalFood = 0;
+        for (var i = 0; i < str.length; i++) {
+          totalFood += str.charCodeAt(i);
+        }
+        return totalFood;
+    }
     
 }
     
-    
-
-
-
 })();
