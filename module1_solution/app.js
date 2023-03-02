9871fe46881342b6157feba52f65468b51c3f660
lance8831 <leezhenhao88@gmail.com>
2023-03-02 22:50:36 +0900

    Module1_solution

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -8,25 +8,30 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController ($scope){ 
     $scope.foodList = "";
     $scope.numOfFood = 0;
+    $scope.message = "";
     
     $scope.checkFood = function () {
         var totalFood = foodCount($scope.foodList);
         $scope.numOfFood = totalFood;
         if (totalFood == 0){
-            return "Please enter your data first.";
+            showMessage("Please enter your data first.");
         }
-        if (totalFood <=3){
-            return "Enjoy!";
+        else if (totalFood <=3){
+            showMessage("Enjoy!");
         }
         else {
-            return "Too much!";
+            showMessage("Too much!");
         }
+    };
+    
+    var showMessage = function(message){
+        $scope.message = message;
     };
     
     function foodCount(str){
         var count = str ? str.split(/,/): 0;
         return count ? count.length : '';
-    };
+    }
 
 }
     
